ea9d87673e3dccb5f224651ecbacb6abbbeaae5d
ryo <b1640342@planet.kanazawa-it.ac.jp>
2016-11-26 19:40:05 +0900

    ファイル分割、関数ポインタ

---
+++ src/assignment.cpp
@@ -0,0 +1,17 @@
+#include "ros_assignment/assignment.hpp"
+
+Robot::Robot(){
+	time = 0;
+	speed = 0;
+	distance = 0;
+}
+
+void Robot::move(){
+	distance = time * speed;
+}
+
+void Robot::speakDistance(int a){
+
+	std::cout<<"robot"<< a <<"が進んだ距離は"<<distance<<"mです(o_o;)"<<std::endl;
+}
+
